a7b004b83c757fdaf778be4d7991bf38cb973dee
Svetlana Bogdan <123helga17@gmail.com>
2026-04-09 22:17:18 +0300

    Add Postman Echo POST test

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnPostData() {
+        RestAssured.baseURI = "https://postman-echo.com";
+
+        given()
+                .contentType("text/plain; charset=UTF-8")  // указываем тип контента
+                .body("some data")                          // тело запроса
+                .when()
+                .post("/post")                              // POST-запрос
+                .then()
+                .statusCode(200)                            // проверка статуса
+                .body("data", equalTo("some data"));        // проверка тела (JSONPath)
+    }
+}
